fa1ddf4c888566512486378aca28946db58758d2
BraDypUS <developer@bradypus.net>
2019-08-26 23:21:30 +0200

    CDN & simplification

--- js/main.js
+++ js/main.js
@@ -198,40 +198,6 @@ jQuery(document).ready(function($) {
   };
   siteCarousel();
 
-  var siteStellar = function() {
-    $(window).stellar({
-      responsive: false,
-      parallaxBackgrounds: true,
-      parallaxElements: true,
-      horizontalScrolling: false,
-      hideDistantElements: false,
-      scrollProperty: 'scroll'
-    });
-  };
-  siteStellar();
-
-  var siteCountDown = function() {
-
-    $('#date-countdown').countdown('2020/10/10', function(event) {
-      var $this = $(this).html(event.strftime('' +
-        '<span class="countdown-block"><span class="label">%w</span> weeks </span>' +
-        '<span class="countdown-block"><span class="label">%d</span> days </span>' +
-        '<span class="countdown-block"><span class="label">%H</span> hr </span>' +
-        '<span class="countdown-block"><span class="label">%M</span> min </span>' +
-        '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
-    });
-
-  };
-  siteCountDown();
-
-  var siteDatePicker = function() {
-
-    if ( $('.datepicker').length > 0 ) {
-      $('.datepicker').datepicker();
-    }
-
-  };
-  siteDatePicker();
 
   var siteSticky = function() {
     $(".js-sticky-header").sticky({topSpacing:0});
